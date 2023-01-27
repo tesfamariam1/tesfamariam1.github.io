d43f9da212d0eb971a60487abae5d9dfc002f556
tesfamariam1 <tesfumerry16@gmail.com>
2023-01-27 15:50:22 +0300

    new Post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230122.003351';
+const cacheName = 'chirpy-20230127.154739';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
